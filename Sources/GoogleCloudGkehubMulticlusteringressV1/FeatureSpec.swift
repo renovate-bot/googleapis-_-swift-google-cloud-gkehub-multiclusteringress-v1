ba9700b70ce081e4e4d44faa5c7fa03a8049e12d
Carlos O'Ryan <coryan@google.com>
2026-07-20 14:41:59 -0400

    chore: update librarian circa 2026-07-20 (#455)
    In this version of librarian:
    
    - Enums with duplicate numeric values get a `case` when converting from
    integers. Notably, this reordered a lot of lines in the generated code.
    - A number of unqualified `String` types become `Swift.String`, as they
    should be.
    - Paginated APIs that reference external items add some missing imports.
    No code is affected because this was blocking generation for libraries
    supporting such APIs.

--- Sources/GoogleCloudGkehubMulticlusteringressV1/FeatureSpec.swift
+++ Sources/GoogleCloudGkehubMulticlusteringressV1/FeatureSpec.swift
@@ -42,7 +42,7 @@ public struct FeatureSpec: Codable, Equatable, GoogleCloudWkt._AnyPackable,
     return copy
   }
 
-  public static var _anyTypeUrl: String {
+  public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.cloud.gkehub.multiclusteringress.v1.FeatureSpec"
   }
   public init(fromAny any: GoogleCloudWkt.`Any`) throws {
