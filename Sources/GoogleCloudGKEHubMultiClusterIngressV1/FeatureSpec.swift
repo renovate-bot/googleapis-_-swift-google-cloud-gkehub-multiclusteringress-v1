bb216e0f0f579c45082a2f9b6698bad0d0496a96
Carlos O'Ryan <coryan@google.com>
2026-08-19 22:21:47 +0000

    chore: update librarian circa 2026-08-18 (#470)
    In this version of librarian we can automatically import some packages
    with a `@_spi()` attribute. That is handy to hide implementation details
    within `GoogleCloudWkt` and `GoogleCloudGax`.

--- Sources/GoogleCloudGKEHubMultiClusterIngressV1/FeatureSpec.swift
+++ Sources/GoogleCloudGKEHubMultiClusterIngressV1/FeatureSpec.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// **Multi-cluster Ingress**: The configuration for the MultiClusterIngress
 /// feature.
